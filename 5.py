386d252489618e0a99b5d253d5b2d11ff14bc588
student <sergeenko.saveli@gmail.com>
2024-02-15 18:54:27 +0200

    init

---
+++ 5.py
@@ -0,0 +1,7 @@
+number = int(input("Ведіть число яке буде множитися: "))
+mnogenya = number * 11
+
+for i in range(number, mnogenya, 7):
+    i
+for s in range(1, 11, 1):
+    print(f"{number} * {s} = {i}")
